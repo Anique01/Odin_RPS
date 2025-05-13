47d4684a4e8a7ef25198004bc5814b1a3725eaa7
Anique01 <faniquemichels812@gmail.com>
2025-05-13 11:51:35 +0200

    Add playground, but something is fun. Need to troubleshoot

--- script.js
+++ script.js
@@ -18,5 +18,24 @@ function getHumanChoice() {
     return humanChoice;
 }
 
+function playRound(humanChoice, getComputerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice === computerChoice) {
+        console.log{"It's a tie"};
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        humanScore += 1;
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    } else {
+        computerScore += 1;
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    }
+}
+
 console.log(getComputerChoice());
 console.log(getHumanChoice());
+playRound(humanSelection, computerSelection);
